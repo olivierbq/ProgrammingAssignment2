0b7003aff1e77a66ce9d2b63852bc60e338660d2
Olivier Bertels <olivierbertels@MBPvanOlivier2.lan>
2016-10-11 15:24:49 +0200

    Finnished Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeMatrixr creates a special "vector", which is really a list containing a function to
+# set the value of the Matrix
+# get the value of the Matrix
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
+#Checks if Matrix is already filled, if not caches data otherwise gets cached data
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get() 
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
